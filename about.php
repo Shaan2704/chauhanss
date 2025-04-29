dcdb0e2b2bc1772960a20d7550b9c663e636625e
Shaan2704 <aishantanu27@gmail.com>
2025-04-29 16:48:05 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title><?php echo "Welcome to My Site"; ?></title>
+</head>
+<body>
+  <h1>Hello, <?php echo "visitor!"; ?></h1>
+  <p>Today's date is <?php echo date("Y-m-d"); ?></p>
+</body>
+</html>
